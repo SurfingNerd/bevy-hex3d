5c56642f7d77cdb5b1a2f72e43bcbdf692e37b43
Thomas Haller <thomashaller@gmx.at>
2023-03-21 12:09:16 +0000

    ticka fascade plugin

--- src/ticka_fascade_plugin.rs
+++ src/ticka_fascade_plugin.rs
@@ -1,5 +1,5 @@
 use bevy::prelude::{Plugin, ResMut};
-use bevy_ticka::ticka_plugin::{TickaPlugin, TickaRes};
+use bevy_ticka::{ticka_plugin::{TickaPlugin, TickaRes}, movement_reader::MovementReader};
 use ticka::real_time_ticka_fascade::RealTimeTickaFascade;
 
 
@@ -8,17 +8,25 @@ struct TickaFascadePlugin {
 
 }
 
-fn ticka_system(real_time_ticka_fascade: ResMut<TickaRes> ) {
+fn ticka_system(mut ticka: ResMut<TickaRes> ) {
 
     //real_time_ticka_fascade.
+    ticka.as_mut().real_time_ticka().
 }
 
 impl Plugin for TickaFascadePlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
 
-        let plugin = TickaPlugin::new();
+
+        let movement_reader = MovementReader::create_with_sender();
+
+        
+
+        let plugin = TickaPlugin::new(movement_reader.create_sender());
 
         plugin.build(app);
+        
+        app.add_system(ticka_system);
         
         app.insert_resource(resource);
     }
